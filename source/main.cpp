c02aed1daac0e86922151bf7629a4abbca0c2352
Maschell <Maschell@gmx.de>
2025-01-30 18:30:55 +0100

    Bump version

--- source/main.cpp
+++ source/main.cpp
@@ -13,7 +13,7 @@ WUMS_MODULE_EXPORT_NAME("homebrew_basemodule");
 WUMS_MODULE_SKIP_INIT_FINI();
 WUMS_DEPENDS_ON(homebrew_functionpatcher);
 
-#define VERSION "v0.2.5"
+#define VERSION "v0.2.6"
 
 WUMS_INITIALIZE(args) {
     initLogging();
